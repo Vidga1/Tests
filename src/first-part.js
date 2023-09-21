2206113b3008ba234a2850465b4cbca9072db71e
Vladislav <vidga471@gmail.com>
2023-09-21 20:14:16 +0300

    check which month will be shown when entering the number

--- src/first-part.js
+++ src/first-part.js
@@ -51,3 +51,51 @@ export function findMax(a, b) {
 		window.console.log(b);
 	}
 }
+
+/* 5. Запросить у пользователя ввод числа от 1 до 12.
+Вывести в консоль название месяца, соответствующее этому 
+числу (1 — январь, 2 — февраль и т.д.). */
+export function getMonthName() {
+	const monthNumber = Number(prompt("Введите число от 1 до 12:"));
+	switch (monthNumber) {
+	case 1:
+		window.console.log('Январь');
+		break;
+	case 2:
+		window.console.log('Февраль');
+		break;
+	case 3:
+		window.console.log('Март');
+		break;
+	case 4:
+		window.console.log('Апрель');
+		break;
+	case 5:
+		window.console.log('Май');
+		break;
+	case 6:
+		window.console.log('Июнь');
+		break;
+	case 7:
+		window.console.log('Июль');
+		break;
+	case 8:
+		window.console.log('Август');
+		break;
+	case 9:
+		window.console.log('Сентябрь');
+		break;
+	case 10:
+		window.console.log('Октябрь');
+		break;
+	case 11:
+		window.console.log('Ноябрь');
+		break;
+	case 12:
+		window.console.log('Декабрь');
+		break;
+	default:
+		window.console.log('Некорректное число');
+		break;
+	}
+}
--- src/first-part.test.js
+++ src/first-part.test.js
@@ -1,7 +1,8 @@
 import { multiplyAndSum,
 	countTotalCharacters,
 	sumOfDigits,
-	findMax } from "./first-part";
+	findMax,
+	getMonthName } from "./first-part";
 
 // 1 задание
 test("multiplyAndSum должен вывести верную сумму и произведение чисел", () => {
@@ -64,5 +65,113 @@ describe('findMax', () => {
 		const mockConsoleLog = jest.spyOn(console, 'log');
 		findMax(2, 4);
 		expect(mockConsoleLog).toHaveBeenCalledWith(4);
+	});
+});
+
+// 5 задание
+
+describe("getMonthName", () => {
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("1");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Январь");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("2");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Февраль");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("3");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Март");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("4");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Апрель");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("5");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Май");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("6");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Июнь");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("7");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Июль");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("8");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Август");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("9");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Сентябрь");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("10");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Октябрь");
+	});
+  
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("11");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Ноябрь");
+	});
+  
+    
+	it("должен возвращать правильное название месяца", () => {
+		// Mock the prompt function
+		window.prompt = jest.fn().mockReturnValueOnce("12");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Декабрь");
+	});
+  
+	it('должен возвращать некорректное число', () => {
+		window.prompt = jest.fn().mockReturnValueOnce("13");
+		const consoleSpy = jest.spyOn(console, "log");
+		getMonthName();
+		expect(consoleSpy).toHaveBeenCalledWith("Некорректное число");
 	});
 });
