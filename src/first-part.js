49e92978eff38150a2eb0b49017d20c154d33f2c
Vladislav <vidga471@gmail.com>
2023-09-21 21:05:48 +0300

    check find min and max numbers in array

--- src/first-part.js
+++ src/first-part.js
@@ -197,3 +197,19 @@ export function multiplyByTwo(arr) {
 	}
 	return result;
 }
+
+/* 15. Найдите и выведите в консоль наибольший и наименьший элемент исходного массива. */
+
+export function findMinMax(arr) {
+	let min = arr[0];
+	let max = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	window.console.log(min, max);;
+};
--- src/first-part.test.js
+++ src/first-part.test.js
@@ -11,7 +11,8 @@ import { multiplyAndSum,
 	createAdminUser,
 	printAdminDetails, 
 	ArraySum,
-	multiplyByTwo} from "./first-part";
+	multiplyByTwo,
+	findMinMax} from "./first-part";
 
 // 1 задание
 test("multiplyAndSum должен вывести верную сумму и произведение чисел", () => {
@@ -284,4 +285,26 @@ test("умножить каждый элемент массива на 2", () =>
 	const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	const b = multiplyByTwo(a);
 	expect(b).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
+});
+
+// 15 задание
+test('вывод минимума и максимума из 10 чисел', () => {
+	const consoleLogSpy = jest.spyOn(console, 'log');
+	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	findMinMax(arr);
+	expect(consoleLogSpy).toHaveBeenCalledWith(1, 10);
+});
+  
+test("вывод минимума и максимума из 1 числа", () => {
+	const consoleLogSpy = jest.spyOn(console, 'log')
+	const arr = [5];
+	findMinMax(arr);
+	expect(consoleLogSpy).toHaveBeenCalledWith(5, 5);
+});
+  
+test("вывод минимума и максимума из отрицательных чисел", () => {
+	const consoleLogSpy = jest.spyOn(console, 'log')
+	const arr = [-1, -2, -3, -4, -5];
+	findMinMax(arr);
+	expect(consoleLogSpy).toHaveBeenCalledWith(-5, -1);
 });
