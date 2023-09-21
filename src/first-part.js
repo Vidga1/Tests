e1a6dda90703b631da7ffd4e0e7eb4e65af3e9cb
Vladislav <vidga471@gmail.com>
2023-09-21 19:44:55 +0300

    check total characters

--- src/first-part.js
+++ src/first-part.js
@@ -9,3 +9,12 @@ export function multiplyAndSum(a, b) {
 	window.console.log("Product:", product);
 	window.console.log("Sum:", sum);
 }
+
+/* 2. В двух переменных хранятся строки символов.
+Написать программу, которая выведет в консоль
+суммарное количество символов в обоих строках */
+
+export function countTotalCharacters(str1, str2) {
+	const totalCharacters = str1.length + str2.length;
+	window.console.log("Total Characters:", totalCharacters);
+}
--- src/first-part.test.js
+++ src/first-part.test.js
@@ -1,4 +1,5 @@
-import { multiplyAndSum } from "./first-part";
+import { multiplyAndSum,
+	countTotalCharacters } from "./first-part";
 
 // 1 задание
 test("multiplyAndSum должен вывести верную сумму и произведение чисел", () => {
@@ -8,5 +9,15 @@ test("multiplyAndSum должен вывести верную сумму и пр
   
 	expect(consoleSpy).toHaveBeenCalledWith("Product:", 6);
 	expect(consoleSpy).toHaveBeenCalledWith("Sum:", 5);
+  
+});
+
+// 2 задание
+test('countTotalCharacters должен вывести верное количество символов строки', () => {
+	const consoleSpy = jest.spyOn(console, 'log');
+  
+	countTotalCharacters("Hello", "World");
+  
+	expect(consoleSpy).toHaveBeenCalledWith('Total Characters:', 10);
   
 });
