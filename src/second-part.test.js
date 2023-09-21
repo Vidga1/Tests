d91d601e08b296955df18d010be0c4e305455139
Vladislav <vidga471@gmail.com>
2023-09-21 21:45:32 +0300

    check diff numbers

---
+++ src/second-part.test.js
@@ -0,0 +1,17 @@
+import { diff, } from "./second-part";
+
+// 1 задание
+
+test('testDiff', () => {
+	// Проверка разности положительных чисел
+	const result1 = diff(5, 10);
+	expect(result1).toBe(5);
+  
+	// Проверка разности отрицательных чисел
+	const result2 = diff(-10, -5);
+	expect(result2).toBe(5);
+  
+	// Проверка разности положительного и отрицательного чисел
+	const result3 = diff(10, -5);
+	expect(result3).toBe(15);
+});
