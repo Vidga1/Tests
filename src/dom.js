b9beb773eb9aa0daa68f12ea7299af583f911046
Vladislav <vidga471@gmail.com>
2023-09-21 18:46:42 +0300

    add eslint and prettier

--- src/dom.js
+++ src/dom.js
@@ -1,28 +1,28 @@
 export function initializePage() {
-    var submitButton = document.getElementById("submitButton");
-    var inputField = document.getElementById("inputField");
+	const submitButton = document.getElementById("submitButton");
+	const inputField = document.getElementById("inputField");
   
-    submitButton.style.display = "none";
+	submitButton.style.display = "none";
   
-    inputField.addEventListener("input", function() {
-      if (inputField.value !== "") {
-        submitButton.style.display = "block";
-      } else {
-        submitButton.style.display = "none";
-      }
-    });
+	inputField.addEventListener("input", () => {
+		if (inputField.value !== "") {
+			submitButton.style.display = "block";
+		} else {
+			submitButton.style.display = "none";
+		}
+	});
   
-    submitButton.addEventListener("click", function() {
-      var newParagraph = document.createElement("p");
-      newParagraph.textContent = inputField.value;
-      document.body.appendChild(newParagraph);
+	submitButton.addEventListener("click", () => {
+		const newParagraph = document.createElement("p");
+		newParagraph.textContent = inputField.value;
+		document.body.appendChild(newParagraph);
   
-      var paragraphs = document.getElementsByTagName("p");
-      if (paragraphs.length > 5) {
-        paragraphs[0].remove();
-      }
+		const paragraphs = document.getElementsByTagName("p");
+		if (paragraphs.length > 5) {
+			paragraphs[0].remove();
+		}
   
-      inputField.value = "";
-      submitButton.style.display = "none";
-    });
-  }
+		inputField.value = "";
+		submitButton.style.display = "none";
+	});
+}
