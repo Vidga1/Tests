22fef82e52c6da4c4e4fc4a4a55ddc8f970f11fc
Vladislav <vidga471@gmail.com>
2023-09-28 07:02:36 +0300

    check sumOfDigits

--- src/task-1.js
+++ src/task-1.js
@@ -10,3 +10,21 @@ export function countTotalCharacters(str1, str2) {
 	const totalCharacters = str1.length + str2.length;
 	window.console.log("Total Characters:", totalCharacters);
 }
+
+export function sumOfDigits() {
+	// eslint-disable-next-line no-alert
+	const number = prompt("Введите трехзначное число:");
+  
+	if (number.length !== 3) {
+		window.console.log("Вы ввели неверное число!");
+		return;
+	}
+  
+	const digit1 = Number(number.charAt(0));
+	const digit2 = Number(number.charAt(1));
+	const digit3 = Number(number.charAt(2));
+  
+	const sum = digit1 + digit2 + digit3;
+  
+	window.console.log("Сумма цифр введенного числа:", sum);
+}
