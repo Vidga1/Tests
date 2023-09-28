6e4124f641cfd980167ca4eaecef771d56b7f6f8
Vladislav <vidga471@gmail.com>
2023-09-28 06:54:08 +0300

    check multiplyAndSum

---
+++ src/task-1.js
@@ -0,0 +1,7 @@
+export function multiplyAndSum(a, b) {
+	const product = a * b;
+	const sum = a + b;
+  
+	window.console.log("Product:", product);
+	window.console.log("Sum:", sum);
+}
---
+++ tests/task-1.test.js
@@ -0,0 +1,11 @@
+import { multiplyAndSum } from "../src/task-1"
+
+test("multiplyAndSum должен вывести верную сумму и произведение чисел", () => {
+	const consoleSpy = jest.spyOn(console, "log");
+  
+	multiplyAndSum(2, 3);
+  
+	expect(consoleSpy).toHaveBeenCalledWith("Product:", 6);
+	expect(consoleSpy).toHaveBeenCalledWith("Sum:", 5);
+  
+});
