b6123bc4281ff3ffa0aa789ac00a1faee59d7bb7
Vladislav <vidga471@gmail.com>
2023-09-28 07:46:09 +0300

    check diff

---
+++ tests/task-6.test.js
@@ -0,0 +1,15 @@
+import { diff } from "../src/task-6"
+
+test('testDiff', () => {
+	// Проверка разности положительных чисел
+	const result1 = diff(5, 10);
+	expect(result1).toBe(5);
+  
+	// Проверка разности отрицательных чисел
+	const result2 = diff(-10, -5);
+	expect(result2).toBe(5);
+  
+	// Проверка разности положительного и отрицательного чисел
+	const result3 = diff(10, -5);
+	expect(result3).toBe(15);
+});
